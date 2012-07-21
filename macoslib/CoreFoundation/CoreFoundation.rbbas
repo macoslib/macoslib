macoslib/macoslib

commit d7f548ea10a8eb971fb00c1b4ca7cf8107755852

• Renamed functions needlessly containing RB or RS • Improved the System Icon example window • Fixed inefficient code in CreateFromDictionary
Signed-off-by: Stéphane Mons <st.mons.lists@free.fr>

macoslib/CoreFoundation/CoreFoundation.rbbas
@@ -110,8 +110,8 @@ Module CoreFoundation
 	#tag EndExternalMethod
 
 	#tag Method, Flags = &h0
-		Function CFTypeFromRSVariant(theValue as Variant) As CFType
-		  // Transform a simple RS variant into a CFType
+		Function CFTypeFromVariant(theValue as Variant) As CFType
+		  // Transform a simple RB variant into a CFType
 		  
 		  #if TargetMacOS
 		    if theValue=nil then
@@ -119,13 +119,14 @@ Module CoreFoundation
 		    end if
 		    
 		    if theValue.IsArray then
-		      return   CFArray.CreateFromRSObjectsArray( theValue )
+		      return   CFArray.CreateFromObjectsArray( theValue )
+		    end if
+		    
+		    if theValue.IsNumeric then
+		      return  new CFNumber( theValue )
 		    end if
 		    
 		    select case theValue.Type
-		    case  Variant.TypeInteger, Variant.TypeLong, Variant.TypeSingle, Variant.TypeDouble
-		      return  new CFNumber( theValue )
-		      
 		    case  Variant.TypeDate
 		      Return  new CFDate( theValue.DateValue )
 		      
@@ -141,11 +142,13 @@ Module CoreFoundation
 		      
 		    case  Variant.TypeObject
 		      if theValue isa Dictionary then  //Dictionary
-		        return  CFDictionary.CreateFromRSDictionary( Dictionary( theValue ))
+		        return  CFDictionary.CreateFromDictionary( Dictionary( theValue ))
 		        
 		      elseif theValue isa MemoryBlock then  //MemoryBlock
 		        return  new CFData( MemoryBlock( theValue ))
 		        
+		      elseif theValue isa CFType then  //Already a CFTYpe
+		        return   CFType( theValue )
 		      end if
 		      
 		    end select

Examples/SystemIconsExampleWindow.rbfrm
@@ -55,7 +55,7 @@ Begin Window SystemIconsExampleWindow
       LockLeft        =   True
       LockRight       =   ""
       LockTop         =   True
-      RequiresSelection=   ""
+      RequiresSelection=   True
       Scope           =   0
       ScrollbarHorizontal=   ""
       ScrollBarVertical=   True
@@ -82,11 +82,11 @@ Begin Window SystemIconsExampleWindow
       Default         =   True
       Enabled         =   True
       Height          =   20
-      HelpTag         =   ""
+      HelpTag         =   "Resize the icon"
       Index           =   -2147483648
       InitialParent   =   ""
       Italic          =   ""
-      Left            =   464
+      Left            =   463
       LockBottom      =   ""
       LockedInPosition=   False
       LockLeft        =   True
@@ -267,9 +267,7 @@ Begin Window SystemIconsExampleWindow
       Left            =   745
       LockedInPosition=   False
       Scope           =   0
-      TabIndex        =   6
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   9
       Visible         =   True
       Width           =   100
@@ -291,7 +289,6 @@ Begin Window SystemIconsExampleWindow
       SelectionType   =   0
       TabIndex        =   6
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   9
       Visible         =   True
       Width           =   142
@@ -313,7 +310,6 @@ Begin Window SystemIconsExampleWindow
       SelectionType   =   2
       TabIndex        =   7
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   9
       Visible         =   True
       Width           =   71
@@ -335,7 +331,6 @@ Begin Window SystemIconsExampleWindow
       SelectionType   =   2
       TabIndex        =   8
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   9
       Visible         =   True
       Width           =   43
@@ -357,7 +352,6 @@ Begin Window SystemIconsExampleWindow
       SelectionType   =   2
       TabIndex        =   9
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   9
       Visible         =   True
       Width           =   43
@@ -379,7 +373,6 @@ Begin Window SystemIconsExampleWindow
       SelectionType   =   2
       TabIndex        =   10
       TabPanelIndex   =   0
-      TabStop         =   True
       Top             =   499
       Visible         =   True
       Width           =   66
@@ -395,7 +388,7 @@ Begin Window SystemIconsExampleWindow
       Index           =   -2147483648
       InitialParent   =   ""
       Italic          =   ""
-      Left            =   283
+      Left            =   302
       LockBottom      =   ""
       LockedInPosition=   False
       LockLeft        =   True
@@ -406,14 +399,13 @@ Begin Window SystemIconsExampleWindow
       Selectable      =   False
       TabIndex        =   11
       TabPanelIndex   =   0
-      TabStop         =   True
       Text            =   "Your age"
       TextAlign       =   0
       TextColor       =   &h000000
       TextFont        =   "System"
       TextSize        =   0
       TextUnit        =   0
-      Top             =   217
+      Top             =   242
       Transparent     =   False
       Underline       =   ""
       Visible         =   True
@@ -436,7 +428,7 @@ Begin Window SystemIconsExampleWindow
       HelpTag         =   ""
       Index           =   -2147483648
       Italic          =   ""
-      Left            =   352
+      Left            =   371
       LimitText       =   0
       LockBottom      =   ""
       LockedInPosition=   False
@@ -445,7 +437,7 @@ Begin Window SystemIconsExampleWindow
       LockTop         =   True
       Mask            =   ""
       Password        =   ""
-      ReadOnly        =   ""
+      ReadOnly        =   True
       Scope           =   0
       TabIndex        =   12
       TabPanelIndex   =   0
@@ -455,7 +447,7 @@ Begin Window SystemIconsExampleWindow
       TextFont        =   "System"
       TextSize        =   0
       TextUnit        =   0
-      Top             =   215
+      Top             =   240
       Underline       =   ""
       UseFocusRing    =   True
       Visible         =   True
@@ -470,10 +462,10 @@ Begin Window SystemIconsExampleWindow
       Enabled         =   True
       EraseBackground =   True
       Height          =   16
-      HelpTag         =   ""
+      HelpTag         =   "Invalid value for your age"
       Index           =   -2147483648
       InitialParent   =   ""
-      Left            =   424
+      Left            =   444
       LockBottom      =   ""
       LockedInPosition=   False
       LockLeft        =   True
@@ -483,7 +475,7 @@ Begin Window SystemIconsExampleWindow
       TabIndex        =   13
       TabPanelIndex   =   0
       TabStop         =   True
-      Top             =   218
+      Top             =   242
       UseFocusRing    =   True
       Visible         =   True
       Width           =   16
@@ -495,7 +487,7 @@ Begin Window SystemIconsExampleWindow
       DataSource      =   ""
       Enabled         =   True
       Height          =   16
-      HelpTag         =   ""
+      HelpTag         =   "Select an icon in the list"
       Index           =   -2147483648
       InitialParent   =   ""
       Italic          =   ""
@@ -510,7 +502,6 @@ Begin Window SystemIconsExampleWindow
       Selectable      =   False
       TabIndex        =   14
       TabPanelIndex   =   0
-      TabStop         =   True
       Text            =   "Colored icons (Templates only)"
       TextAlign       =   0
       TextColor       =   &h000000
@@ -540,7 +531,7 @@ Begin Window SystemIconsExampleWindow
       TabIndex        =   15
       TabPanelIndex   =   0
       TabStop         =   True
-      Top             =   255
+      Top             =   283
       Visible         =   True
       Width           =   400
    End
@@ -566,14 +557,13 @@ Begin Window SystemIconsExampleWindow
       Selectable      =   False
       TabIndex        =   16
       TabPanelIndex   =   0
-      TabStop         =   True
       Text            =   "All the icons in this window have been obtained from the convenience SystemIcons module. They are all simple Pictures, e.g.\r\rpict = SystemIcons.Network\r\rfor the Network icon in the toolbar."
       TextAlign       =   0
       TextColor       =   &h000000
       TextFont        =   "System"
       TextSize        =   0
       TextUnit        =   0
-      Top             =   265
+      Top             =   293
       Transparent     =   False
       Underline       =   ""
       Visible         =   True
@@ -601,7 +591,6 @@ Begin Window SystemIconsExampleWindow
       Selectable      =   False
       TabIndex        =   17
       TabPanelIndex   =   0
-      TabStop         =   True
       Text            =   "Warning: you need Real Studio 2011.r4 or higher and Mac OS X 10.6 or higher for optimum results (especially transparency)."
       TextAlign       =   0
       TextColor       =   &h000000
@@ -613,6 +602,61 @@ Begin Window SystemIconsExampleWindow
       Underline       =   ""
       Visible         =   True
       Width           =   401
+   End
+   Begin Label Label5
+      AutoDeactivate  =   True
+      Bold            =   ""
+      DataField       =   ""
+      DataSource      =   ""
+      Enabled         =   True
+      Height          =   20
+      HelpTag         =   ""
+      Index           =   -2147483648
+      InitialParent   =   ""
+      Italic          =   True
+      Left            =   283
+      LockBottom      =   ""
+      LockedInPosition=   False
+      LockLeft        =   True
+      LockRight       =   ""
+      LockTop         =   True
+      Multiline       =   ""
+      Scope           =   0
+      Selectable      =   False
+      TabIndex        =   18
+      TabPanelIndex   =   0
+      Text            =   "Example of a bad entry signalled by a red arrow:"
+      TextAlign       =   0
+      TextColor       =   &h000000
+      TextFont        =   "System"
+      TextSize        =   0
+      TextUnit        =   0
+      Top             =   214
+      Transparent     =   False
+      Underline       =   ""
+      Visible         =   True
+      Width           =   375
+   End
+   Begin Separator Separator2
+      AutoDeactivate  =   True
+      Enabled         =   True
+      Height          =   4
+      HelpTag         =   ""
+      Index           =   -2147483648
+      InitialParent   =   ""
+      Left            =   273
+      LockBottom      =   ""
+      LockedInPosition=   False
+      LockLeft        =   True
+      LockRight       =   ""
+      LockTop         =   True
+      Scope           =   0
+      TabIndex        =   19
+      TabPanelIndex   =   0
+      TabStop         =   True
+      Top             =   198
+      Visible         =   True
+      Width           =   400
    End
 End
 #tag EndWindow
@@ -665,6 +709,8 @@ End
 		  next
 		  
 		  #pragma BreakOnExceptions default
+		  
+		  LB1.ListIndex = 0
 		End Sub
 	#tag EndEvent
 	#tag Event

macoslib.rbvcp
@@ -218,7 +218,7 @@ Window=AlphaWindow;Examples/AlphaWindow.rbfrm;&h6D9FD2A0;&h4BD59C08;false
 Window=NSColorWindow;Examples/NSColorWindow.rbfrm;&hCE4568F;&h4BD59C08;false
 Window=NSDatePickerWindow;Examples/NSDatePickerWindow.rbfrm;&h1A182CDC;&h4BD59C08;false
 Class=NSObject;macoslib/Cocoa/NSObject.rbbas;&h2FC38E1C;&h36D6C6CC;false
-Class=NSObjectRBWrapper;macoslib/Cocoa/NSObjectRBWrapper.rbbas;&h2E2FE7BA;&h36D6C6CC;false
+Class=NSWrapperForObject;macoslib/Cocoa/NSWrapperForObject.rbbas;&h2E2FE7BA;&h36D6C6CC;false
 Class=NSPathComponentCell;macoslib/Cocoa/NSPathComponentCell.rbbas;&h29296E8C;&h36D6C6CC;false
 Class=NSPathControl;macoslib/Cocoa/NSPathControl.rbbas;&h3CA43C11;&h36D6C6CC;false
 Class=NSParagraphStyle;macoslib/Cocoa/NSParagraphStyle.rbbas;&h705018ED;&h36D6C6CC;false

macoslib/Cocoa/Cocoa.rbbas
@@ -710,9 +710,9 @@ Protected Module Cocoa
 	#tag EndMethod
 
 	#tag Method, Flags = &h1
-		Protected Function NSObjectFromRSVariant(v as variant) As variant
+		Protected Function NSObjectFromVariant(v as variant) As variant
 		  if v.IsArray then
-		    return   NSArray.CreateFromRSObjectsArray( v )
+		    return   NSArray.CreateFromObjectsArray( v )
 		  end if
 		  
 		  select case v.Type
@@ -730,7 +730,7 @@ Protected Module Cocoa
 		    
 		  case Variant.TypeObject  //->Dictionary, MemoryBlock
 		    if v IsA Dictionary then
-		      return   NSDictionary.CreateFromRSDictionary( Dictionary( v ))
+		      return   NSDictionary.CreateFromDictionary( Dictionary( v ))
 		    elseif v IsA MemoryBlock then
 		      return   new NSData( MemoryBlock( v ))
 		    elseif v isa NSObject then //Already a NSObject

macoslib/Cocoa/NSArray.rbbas
@@ -48,7 +48,7 @@ Inherits NSObject
 	#tag EndMethod
 
 	#tag Method, Flags = &h0
-		 Shared Function CreateFromRSObjectsArray(theArray as variant) As NSArray
+		 Shared Function CreateFromObjectsArray(theArray as variant) As NSArray
 		  dim nsma as new NSMutableArray
 		  
 		  select case theArray.ArrayElementType
@@ -85,7 +85,7 @@ Inherits NSObject
 		  case 9
 		    dim arv() as variant = theArray
 		    for each v as variant in arv
-		      nsma.Append   NSArray.CreateFromRSObjectsArray( v )
+		      nsma.Append   NSArray.CreateFromObjectsArray( v )
 		    next
 		  end select
 		  

macoslib/Cocoa/NSAttributedString.rbbas
@@ -67,7 +67,7 @@ Inherits NSObject
 		    declare function initWithString lib CocoaLib selector "initWithString:attributes:" (id as Ptr, theString as CFStringRef, attr as Ptr) as Ptr
 		    
 		    dim p as Ptr
-		    dim NSDict as NSDictionary = NSDictionary.CreateFromRSDictionary( attr )
+		    dim NSDict as NSDictionary = NSDictionary.CreateFromDictionary( attr )
 		    p = NSAttributedString.Allocate( "NSAttributedString" )
 		    p = initWithString( p, value, NSDict )
 		    

macoslib/Cocoa/NSDictionary.rbbas
@@ -51,6 +51,25 @@ Inherits NSObject
 	#tag EndMethod
 
 	#tag Method, Flags = &h0
+		 Shared Function CreateFromDictionary(dict as Dictionary) As NSDictionary
+		  #if TargetMacOS
+		    dim md as new NSMutableDictionary
+		    
+		    if dict=nil then
+		      return   nil
+		    end if
+		    
+		    for i as integer=0 to dict.Count - 1
+		      md.Value( Cocoa.NSObjectFromVariant( dict.Key( i ))) = Cocoa.NSObjectFromVariant( dict.value( dict.key( i )))
+		    next
+		    
+		    return   md.Copy
+		    
+		  #endif
+		End Function
+	#tag EndMethod
+
+	#tag Method, Flags = &h0
 		 Shared Function CreateFromFile(file as FolderItem) As NSDictionary
 		  //# Create a new NSDictionary from a file (like as .plist file)
 		  
@@ -62,25 +81,6 @@ Inherits NSObject
 		    if p<>nil then
 		      return   new NSDictionary( p, false )
 		    end if
-		    
-		  #endif
-		End Function
-	#tag EndMethod
-
-	#tag Method, Flags = &h0
-		 Shared Function CreateFromRSDictionary(dict as Dictionary) As NSDictionary
-		  #if TargetMacOS
-		    dim md as new NSMutableDictionary
-		    
-		    if dict=nil then
-		      return   nil
-		    end if
-		    
-		    for i as integer=0 to dict.Count - 1
-		      md.Value( Cocoa.NSObjectFromRSVariant( dict.Key( i ))) = Cocoa.NSObjectFromRSVariant( dict.value( dict.key( i )))
-		    next
-		    
-		    return   md.Copy
 		    
 		  #endif
 		End Function

macoslib/Cocoa/NSNetService.rbbas
@@ -782,7 +782,7 @@ Inherits NSObject
 			Set
 			  #if TargetMacOS
 			    dim data as NSData
-			    dim nsd as NSDictionary = NSDictionary.CreateFromRSDictionary( value )
+			    dim nsd as NSDictionary = NSDictionary.CreateFromDictionary( value )
 			    
 			    data = NSNetService.DataFromTXTRecordDictionary( nsd )
 			    

commit ecce94d4a03f755eae5e083e030ee64c24b7d250

• Reverted use of IsNumeric in commit d7f548ea10a8eb971fb00c1b4ca7cf8107755852 which caused problems with strings
Signed-off-by: Stéphane Mons <st.mons.lists@free.fr>

macoslib/CoreFoundation/CoreFoundation.rbbas
@@ -122,11 +122,10 @@ Module CoreFoundation
 		      return   CFArray.CreateFromObjectsArray( theValue )
 		    end if
 		    
-		    if theValue.IsNumeric then
-		      return  new CFNumber( theValue )
-		    end if
-		    
 		    select case theValue.Type
+		    case  Variant.TypeInteger, Variant.TypeLong, Variant.TypeDouble, Variant.TypeSingle
+		      Return   new CFNumber( theValue )
+		      
 		    case  Variant.TypeDate
 		      Return  new CFDate( theValue.DateValue )
 		      